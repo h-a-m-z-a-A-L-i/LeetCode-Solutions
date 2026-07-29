f7822ee396ddcf0afd7fd0dbc693b59cf2ef5474
Hamza_Ali <154522628+h-a-m-z-a-A-L-i@users.noreply.github.com>
2026-07-29 22:45:49 +0500

    Time: 3 ms (36.1%), Space: 19.2 MB (96.22%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -5,27 +5,22 @@ class Solution:
         sign = 1
         num = 0
 
-        # 1. Skip leading whitespace
         while i < n and s[i] == ' ':
             i += 1
 
-        # 2. Check sign
         if i < n and s[i] == '-':
             sign = -1
             i += 1
         elif i < n and s[i] == '+':
             i += 1
 
-        # 3. Convert digits iteratively without joining strings
         while i < n and s[i].isdigit():
             digit = ord(s[i]) - ord('0')
             num = num * 10 + digit
             i += 1
 
-        # 4. Apply sign
         num *= sign
 
-        # 5. Handle 32-bit signed integer overflow limits
         INT_MIN, INT_MAX = -2**31, 2**31 - 1
         if num < INT_MIN:
             return INT_MIN
