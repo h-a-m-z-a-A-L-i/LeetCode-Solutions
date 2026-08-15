95f50a5dadc8e46b27df25ecc6610d4961071dff
Hamza_Ali <154522628+h-a-m-z-a-A-L-i@users.noreply.github.com>
2026-08-15 21:08:56 +0500

    Time: 7 ms (40.45%), Space: 19.5 MB (19.43%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        dt = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        return sum(dt[char] for char in s)
+
+            
