24a985d23f4f8d6bbd426d0a2122611963032f7a
Hamza_Ali <154522628+h-a-m-z-a-A-L-i@users.noreply.github.com>
2026-09-02 17:08:17 +0500

    Time: 7 ms (39.23%), Space: 20.3 MB (89.92%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+
+        write_ptr = 0
+
+        for read_ptr in range(len(nums)):
+            if nums[read_ptr] != 0:
+                nums[write_ptr], nums[read_ptr] = nums[read_ptr], nums[write_ptr]
+                write_ptr += 1
